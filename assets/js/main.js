ffa0c185f72a0386c7dfced65872ae98f149f261
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 15:59:11 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var txt = "How inappropriate to call this planet Earth, when clearly it is Ocean."
+
+console.log(txt.indexOf("h"));
+console.log(txt.indexOf("Earth"));
+console.log(txt.indexOf("Moon"));/* Das Ergbniss ist -1,
+ da wir das Wort "Moon"im zugewiesenen Text nicht haben */
+
